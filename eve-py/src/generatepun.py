56da4762e0b19801598509d0e10046a9e4d3409c
zuodap47 <118099461+zuodap47@users.noreply.github.com>
2025-03-29 14:37:11 +0000

    Update generatepun.py

--- eve-py/src/generatepun.py
+++ eve-py/src/generatepun.py
@@ -33,33 +33,51 @@ def ttpg2gm(TTPG,pl):
     f.close()
     
 
-def compute_pun(pl_name,PUN,TTPG):
-    '''From TTPG to PGSolver representation'''
-    ttpg2gm(TTPG,pl_name)
+
+def compute_pun(pl_name, PUN, TTPG):
+    '''从 TTPG 转换为 Oink 格式'''
+    ttpg2gm(TTPG, pl_name)
     
-    '''Call PGSolver subroutine to compute Pun_i'''
-    lines = sp.Popen(['/home/mirror/.opam/4.07.0/bin/pgsolver-bin', '-global', 'recursive', '../temp/ttpg_%s' % pl_name], stdout=sp.PIPE, stderr=sp.PIPE, encoding='utf8')
-
-    out, err = lines.communicate()
-
-    ttpg_results={}
-    out = out.splitlines()
-    for line in out:
-        word = line.split(" ")
-        if word[0]=="Player":
-            pl = word[1]
-        for c in list(word):
-#            if line[0]=="Player" and line[1]=="0":
-            try:
-                if c[0]=="{":
-                    ttpg_results[int(pl)]=line
-            except IndexError:
-                pass
-
+    '''调用 Oink 计算 Pun_i'''
+    oink_cmd = [
+        '/usr/local/bin/oink',  # 确保 Oink 路径正确
+        '--solver', 'fpi',      # 选择 fpi 作为求解算法（可以换成其他算法）
+        '--timeout', '300',     # 设定超时（秒）
+        '--print',              # 输出结果到终端
+        '../temp/ttpg_%s' % pl_name  # 输入的 TTPG 文件路径
+    ]
 
     try:
-        PUN[pl_name]=list(map(int,ttpg_results[0].strip()[1:-1].replace(' ','').split(',')))
-    except ValueError:
-        PUN[pl_name]=[]
-    return PUN
+        process = sp.Popen(oink_cmd, stdout=sp.PIPE, stderr=sp.PIPE, encoding='utf8')
+        out, err = process.communicate()
+        
+        if err:
+            print("Oink 错误信息：", err)
 
+        ttpg_results = {}
+        out_lines = out.splitlines()
+        
+        for line in out_lines:
+            words = line.split(" ")
+            if words[0] == "Player":
+                pl = words[1]
+            for c in words:
+                try:
+                    if c[0] == "{":
+                        ttpg_results[int(pl)] = line
+                except IndexError:
+                    pass
+        
+        # Debug 输出，查看 Oink 结果
+        print("Oink 解析结果:", ttpg_results)
+        
+        try:
+            PUN[pl_name] = list(map(int, ttpg_results[0].strip()[1:-1].replace(' ', '').split(',')))
+        except (ValueError, KeyError):
+            PUN[pl_name] = []
+    
+    except FileNotFoundError:
+        print(f"错误：未找到 Oink 可执行文件，请检查路径 {oink_cmd[0]}")
+        PUN[pl_name] = []
+    
+    return PUN
